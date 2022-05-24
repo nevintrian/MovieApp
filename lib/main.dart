2eaf81fb19eb36172e93f44529bb104d94a796f0
Nevin Trian <nevin.trian@suitmedia.com>
2022-05-24 10:27:33 +0700

    add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:movies_app/pages/about/about_page.dart';
 import 'package:movies_app/pages/detail/detail_page.dart';
 import 'package:movies_app/pages/home/home_page.dart';
@@ -9,7 +10,8 @@ import 'package:movies_app/pages/top_rated/top_rated_page.dart';
 import 'package:movies_app/pages/upcoming/upcoming_page.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
       .then((_) {
     runApp(const MyApp());
@@ -25,6 +27,12 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   @override
+  void initState() {
+    initialization();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -37,7 +45,7 @@ class _MyAppState extends State<MyApp> {
         TopRatedPage.routeName: (context) => const TopRatedPage(),
         UpcomingPage.routeName: (context) => const UpcomingPage(),
         // DetailPage.routeName: (context) => const DetailPage(),
-        SearchPage.routeName: (context) => const SearchPage()
+        SearchPage.routeName: (context) => const SearchPage(),
       },
     );
   }
@@ -56,4 +64,19 @@ MaterialPageRoute _buildRoute(RouteSettings settings, Widget builder) {
     settings: settings,
     builder: (ctx) => builder,
   );
+}
+
+void initialization() async {
+  // This is where you can initialize the resources needed by your app while
+  // the splash screen is displayed.  Remove the following example because
+  // delaying the user experience is a bad design practice!
+  // ignore_for_file: avoid_print
+  print('ready in 3...');
+  await Future.delayed(const Duration(seconds: 1));
+  print('ready in 2...');
+  await Future.delayed(const Duration(seconds: 1));
+  print('ready in 1...');
+  await Future.delayed(const Duration(seconds: 1));
+  print('go!');
+  FlutterNativeSplash.remove();
 }
